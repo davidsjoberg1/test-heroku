073c5c654dd0bd08ef4fbbca3d5ef58840c446c3
davidsjoberg1 <96268076+davidsjoberg1@users.noreply.github.com>
2024-08-08 11:54:42 +0200

    second

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from . import db
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
 if __name__ == '__main__':
-    db.drop_all()
-    db.create_all()
+    #db.drop_all()
+    #db.create_all()
     app.debug = True
     app.run()
     #app.run(host='0.0.0.0', port=5012)
