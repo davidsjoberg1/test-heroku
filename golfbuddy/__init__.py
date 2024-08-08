9d2e462f2b8cd433a2ddb2d690534f85cfc33de4
davidsjoberg1 <96268076+davidsjoberg1@users.noreply.github.com>
2024-08-08 12:24:11 +0200

    10

--- golfbuddy/__init__.py
+++ golfbuddy/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-#app.config.from_pyfile("config.py")
-#db = SQLAlchemy(app)
+app.config.from_pyfile("config.py")
+db = SQLAlchemy(app)
 
 #app = Flask(__name__)
 #app.config.from_pyfile("config.py")
