3a8cd078da1935e41f607a6692a120bb42e85455
davidsjoberg1 <96268076+davidsjoberg1@users.noreply.github.com>
2024-08-08 12:44:30 +0200

    16

--- golfbuddy/app.py
+++ golfbuddy/app.py
@@ -1,16 +1,16 @@
 from golfbuddy import app
 from golfbuddy import db
+from golfbuddy import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-from flask import jsonify, request
 
 
 
 
-@app.route('/')
-def hello_world():
-    return jsonify('hello_world'), 200
+
+
+
 
 if __name__ == '__main__':
     db.drop_all()
